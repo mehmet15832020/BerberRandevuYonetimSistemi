ea2c15e19b08a069205a988cc857509943f268df
mehmet15832020 <20mehmetcapar02@gmail.com>
2024-11-26 12:31:37 +0300

    ASP.NET Core MVC projesi başlatıldı

---
+++ BerberRandevuYonetimSistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BerberRandevuYonetimSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
